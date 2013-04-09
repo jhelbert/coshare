394b52f1da01a87961d974ed14cdff8ca430ccb9
Justin Helbert <jhelbert@mit.edu>
2013-04-09 18:09:57 -0400

    django settings set up

--- main/views.py
+++ main/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from main.models import Content,Playlist
+from django.shortcuts import render_to_response
+from django import forms
+from django.template import RequestContext
+
+def main(request):
+	playlists = Playlist.objects.all()
+	return render_to_response('index.html', 
+		{
+		 "playlists":playlists
+		},
+		context_instance=RequestContext(request))# Create your views here.
