a5b500b8dfb2d0b9d766865f0eb484ccddb4aab3
Justin Helbert <jhelbert@mit.edu>
2013-04-17 20:31:43 -0400

    latest

---
+++ main/static/js/content.js
@@ -0,0 +1,55 @@
+/**
+ * ADT representing an individual piece of content
+ * 
+ * TODO  - implement more than just pictures
+ * 
+ * @image - an Image object of the actual media
+ * @id - integer that is database ID
+ */
+var Content = function(id,src,size) {
+    this.id = id;
+
+    // TODO: create checkrep?
+
+    /** @return javascript object of the image */
+    this.get_content = function() {
+
+        // use stubbed image for now
+        var output = new Image();
+        output.src = "/media/" + src;
+        console.log(size);
+        output.height = size*10;
+        return output;
+
+        // TODO: interface with backend
+        // request image object each time it's needed?
+        // cache it?
+    }
+
+    /** sets the description of the image */
+    this.set_description = function(new_description) {
+        // currently does nothing
+
+        // TODO: interface with backend
+    }
+
+    /** @return string description of content */
+    this.get_description = function() {
+
+        // use stubbed description for now
+        return "Isn't this picture awesome!?";
+
+        // TODO: interface with backend
+        // request description each time it's needed
+        // cache it?
+    }
+
+    /** 
+     * @return true iff @other has same id as this piece of content, 
+     *         false otherwise
+     */
+    this.equals = function(other) {
+        return this.id === other.id;
+    }
+    
+}
