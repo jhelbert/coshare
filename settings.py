2f3cfacee41db53b540d22374d76f8f83754484e
Parker Tew <ptew@mit.edu>
2013-05-01 11:10:32 -0400

    uncommenting main

--- settings.py
+++ settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'main',
+    'main',
     'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
